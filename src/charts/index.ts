e4fa451e47ee167187d59330b99b7630df3632db
白宇东 <summer88123@163.com>
2026-03-27 18:23:53 +0800

    feat(charts): register 3 new bug analysis charts in allCharts

--- src/charts/index.ts
+++ src/charts/index.ts
@@ -2,9 +2,15 @@ import { ChartModule } from './chart.interface';
 import { bugByAssigneeChart } from './modules/bug-by-assignee';
 import { bugByDefectAnalysisChart } from './modules/bug-by-defect-analysis';
 import { bugByModuleChart } from './modules/bug-by-module';
+import { bugByFixDurationChart } from './modules/bug-by-fix-duration';
+import { bugByDeveloperHoursChart } from './modules/bug-by-developer-hours';
+import { bugOpenPriorityHeatmapChart } from './modules/bug-open-priority-heatmap';
 
 export const allCharts: ChartModule[] = [
   bugByDefectAnalysisChart,
   bugByAssigneeChart,
   bugByModuleChart,
+  bugByFixDurationChart,
+  bugByDeveloperHoursChart,
+  bugOpenPriorityHeatmapChart,
 ];
